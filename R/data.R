0da5667cab1231299fecb92cabc8805090f271f8
I. Richard Thompson <ithompson@qf.org.qa>
2016-07-24 17:33:34 +0000

    version 0.1.4

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Input Vector of Drug/Gene Data
+#'
+#' A mixed list of drug identifiers and gene names, as output by \code{parseInputFile()}
+#' and used for input into \code{getUniqueGeneList()} and
+#'
+#' @format A vector of 10 drug identification numbers, intersperced with 90 Gene names.
+"inputlist"
+
+#' Input Matrix of Drug/Gene Data
+#'
+#' A matrix made up of a column of drug identifiers and a column of associated gene names, as used for input into \code{getUniqueGeneList()} and
+#'
+#' @format A 2 column matrix where the first columns contains drug identifiers and the second column Gene names.
+"genematrix"
